7b5dd18f74b4fccc5aee8de87c806ce37e8cd530
Alex Levin <alevin@gmail.com>
2021-02-10 16:14:11 -0800

    scale 0.8 , point +7

--- src/main/java/frc/robot/TrajectoryHelper.java
+++ src/main/java/frc/robot/TrajectoryHelper.java
@@ -35,7 +35,7 @@ public class TrajectoryHelper {
     public static double[][] testStep= {
         {0,0},
     //  {120,0},
-        {180,0},
+        {180+7,0},
     
     };
 
@@ -136,7 +136,7 @@ public class TrajectoryHelper {
     };
 
 //    public static double GLOBAL_SCALE = 0.827;// divide size by 5 to fit into Hajel's garage
-      public static double GLOBAL_SCALE = 0.85;// divide size by 5 to fit into Hajel's garage
+      public static double GLOBAL_SCALE = 0.8;// divide size by 5 to fit into Hajel's garage
 
     /**
      * translateAndScale takes an array of integer coordinates in 2-d space, and scales them to meters, and applies a scale in additinoos
