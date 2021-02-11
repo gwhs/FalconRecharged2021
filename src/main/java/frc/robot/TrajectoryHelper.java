7a3abe096d20b3c3c6afc56260fa961301cd21f4
Alex Levin <alevin@gmail.com>
2021-02-10 16:05:45 -0800

    another scale tuning

--- src/main/java/frc/robot/TrajectoryHelper.java
+++ src/main/java/frc/robot/TrajectoryHelper.java
@@ -34,7 +34,7 @@ public class TrajectoryHelper {
 
     public static double[][] testStep= {
         {0,0},
-        {120+6,0},
+        {120,0},
     };
 
  //   public static double[][] testStep= {
@@ -134,7 +134,7 @@ public class TrajectoryHelper {
     };
 
 //    public static double GLOBAL_SCALE = 0.827;// divide size by 5 to fit into Hajel's garage
-      public static double GLOBAL_SCALE = 0.85;// divide size by 5 to fit into Hajel's garage
+      public static double GLOBAL_SCALE = 0.83;// divide size by 5 to fit into Hajel's garage
 
     /**
      * translateAndScale takes an array of integer coordinates in 2-d space, and scales them to meters, and applies a scale in additinoos
