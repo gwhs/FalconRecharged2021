119e1eaa5aa224d84e55bb9a22f0515cbdbe87fe
Alexandre Levin <alexlevin@factset.com>
2021-02-19 22:10:55 -0500

    Increased speed in autonomous and teleop. Corrected wheel size.

--- src/main/java/frc/robot/utility/TrajectoryMaker.java
+++ src/main/java/frc/robot/utility/TrajectoryMaker.java
@@ -49,7 +49,7 @@ public TrajectoryMaker(Pose2d start, Pose2d end, ArrayList<Translation2d> points
     this.end = end;
     this.isHyp = false;
     angle = 0;
-    config = new TrajectoryConfig(0.5, 0.5);
+    config = new TrajectoryConfig(0.7, 0.5);  // (TrajectoryConfig​(double maxVelocityMetersPerSecond, double maxAccelerationMetersPerSecondSq))
     config.setStartVelocity(0);
     config.setEndVelocity(0);
     config.setReversed(false);
