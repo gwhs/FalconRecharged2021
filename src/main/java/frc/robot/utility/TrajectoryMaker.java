4ca8dd7abe136377832269418bd4d4823c0d6f46
Alex Levin <alevin@gmail.com>
2021-03-08 16:01:50 -0800

    trying to increase velocity and accel

--- src/main/java/frc/robot/utility/TrajectoryMaker.java
+++ src/main/java/frc/robot/utility/TrajectoryMaker.java
@@ -23,72 +23,70 @@ import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstr
  */
 public class TrajectoryMaker {
 
-private Pose2d start;
-private Pose2d end;
-private double angle;
-private boolean isHyp;  //what does this mean?
-private TrajectoryConfig config;
-private Trajectory trajectory;
-private ArrayList<Translation2d> listOfPoints;
+    private Pose2d start;
+    private Pose2d end;
+    private double angle;
+    private boolean isHyp; // what does this mean?
+    private TrajectoryConfig config;
+    private Trajectory trajectory;
+    private ArrayList<Translation2d> listOfPoints;
 
-public TrajectoryMaker(Pose2d start, Pose2d end, boolean isHyp) {
-    this.start = start;
-    this.end = end;
-    this.isHyp = isHyp;
-    angle = 0;
-    config = new TrajectoryConfig(4, 2);  //make into constants
-    config.setStartVelocity(0);
-    config.setEndVelocity(0);
-    config.setReversed(false);
-    config.addConstraint(new CentripetalAccelerationConstraint(0.5));
-    listOfPoints = new ArrayList<Translation2d>();
-    trajectory = createTrajectory();
-}
-public TrajectoryMaker(Pose2d start, Pose2d end, ArrayList<Translation2d> points) {
-    this.start = start;
-    this.end = end;
-    this.isHyp = false;
-    angle = 0;
-    config = new TrajectoryConfig(3, 2);  // (TrajectoryConfig​(double maxVelocityMetersPerSecond, double maxAccelerationMetersPerSecondSq))
-    config.setStartVelocity(0);
-    config.setEndVelocity(0);
-    config.setReversed(false);
-    config.addConstraint(new CentripetalAccelerationConstraint(2));
-    listOfPoints = points;
-    trajectory = createTrajectory();
-}
-
-
-private Trajectory createTrajectory()
-{
-    SmartDashboard.putBoolean("isHyp", isHyp);
-    if(isHyp) 
-    {   
-
-        double x_Dis = end.getTranslation().getX() - start.getTranslation().getX();
-        double y_Dis = end.getTranslation().getY() - start.getTranslation().getY();
-        double distance = start.getTranslation().getDistance(end.getTranslation());
-        angle =  90 - Math.toDegrees(Math.atan(x_Dis/y_Dis)); // because coordinate system is different
-        if(y_Dis < 0) angle += 180;
-        Pose2d endPos = new Pose2d(new Translation2d(distance,0), new Rotation2d(0));
-        SmartDashboard.putNumber("angle", angle);
-        SmartDashboard.putNumber("distance", distance);
-        return TrajectoryGenerator.generateTrajectory(start, listOfPoints , endPos, config);
-    }
-    else
-    {
+    public TrajectoryMaker(Pose2d start, Pose2d end, boolean isHyp) {
+        this.start = start;
+        this.end = end;
+        this.isHyp = isHyp;
         angle = 0;
-        return TrajectoryGenerator.generateTrajectory(start, listOfPoints , end, config);
+        config = new TrajectoryConfig(4, 2); // make into constants
+        config.setStartVelocity(0);
+        config.setEndVelocity(0);
+        config.setReversed(false);
+        config.addConstraint(new CentripetalAccelerationConstraint(0.5));
+        listOfPoints = new ArrayList<Translation2d>();
+        trajectory = createTrajectory();
     }
 
-}
+    public TrajectoryMaker(Pose2d start, Pose2d end, ArrayList<Translation2d> points) {
+        this.start = start;
+        this.end = end;
+        this.isHyp = false;
+        angle = 0;
+        config = new TrajectoryConfig(4, 3); // (TrajectoryConfig​(double maxVelocityMetersPerSecond, double
+                                             // maxAccelerationMetersPerSecondSq))
+        config.setStartVelocity(0);
+        config.setEndVelocity(0);
+        config.setReversed(false);
+        config.addConstraint(new CentripetalAccelerationConstraint(3));
+        listOfPoints = points;
+        trajectory = createTrajectory();
+    }
 
-public double getAngle() {
-    return angle;
-}
+    private Trajectory createTrajectory() {
+        SmartDashboard.putBoolean("isHyp", isHyp);
+        if (isHyp) {
 
-public Trajectory getTrajectory() {
-    return trajectory;
-}
+            double x_Dis = end.getTranslation().getX() - start.getTranslation().getX();
+            double y_Dis = end.getTranslation().getY() - start.getTranslation().getY();
+            double distance = start.getTranslation().getDistance(end.getTranslation());
+            angle = 90 - Math.toDegrees(Math.atan(x_Dis / y_Dis)); // because coordinate system is different
+            if (y_Dis < 0)
+                angle += 180;
+            Pose2d endPos = new Pose2d(new Translation2d(distance, 0), new Rotation2d(0));
+            SmartDashboard.putNumber("angle", angle);
+            SmartDashboard.putNumber("distance", distance);
+            return TrajectoryGenerator.generateTrajectory(start, listOfPoints, endPos, config);
+        } else {
+            angle = 0;
+            return TrajectoryGenerator.generateTrajectory(start, listOfPoints, end, config);
+        }
+
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public Trajectory getTrajectory() {
+        return trajectory;
+    }
 
 }
