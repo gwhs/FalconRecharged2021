d59266721610413a6773a15701544490dcdb7b71
leinouye <55515800+leinouye@users.noreply.github.com>
2021-03-20 15:51:11 -0700

    second starting position 0 in bounce

--- src/main/java/frc/robot/TrajectoryHelper.java
+++ src/main/java/frc/robot/TrajectoryHelper.java
@@ -253,7 +253,7 @@ public static double[][] testStep= {
     
     public static TrajectoryMaker createBounce1()
     {
-        return createTrajectory(bounce1, GLOBAL_SCALE, 3 * Math.PI / 2, Math.PI / 2, true);
+        return createTrajectory(bounce1, GLOBAL_SCALE, 0, Math.PI / 2, true);
     } 
 
     public static TrajectoryMaker createBarrel()
