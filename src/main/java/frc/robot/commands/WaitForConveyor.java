2df7a3733fcca11e09ff3bfc1ff31b457e94396a
zhxu033 <77419802+zhxu033@users.noreply.github.com>
2021-03-20 11:44:04 -0700

    fix format

--- src/main/java/frc/robot/commands/WaitForConveyor.java
+++ src/main/java/frc/robot/commands/WaitForConveyor.java
@@ -9,24 +9,27 @@ public class WaitForConveyor extends CommandBase {
     private long startTime;
     private final double maxWaitTime = 1000;
 
-    public WaitForConveyor(ConveyorTalon conveyor)
-    {
+    public WaitForConveyor(ConveyorTalon conveyor) {
         super();
         conveyorTalon = conveyor;
         addRequirements(conveyorTalon);
     }
-    /** The initial subroutine of a command. Called once when the command is initially scheduled. */
-  public void initialize() {
-    startTime = System.currentTimeMillis();
-  }
 
-  //checks if power cell is in conveyor to finish or one second has gone by
-  public boolean isFinished() { 
-    long currentTime = System.currentTimeMillis();
-      if (conveyorTalon.getHasSeen() || (currentTime - startTime)>maxWaitTime){
-        return true;
-      }
-    return false;
-  }
+    /**
+     * The initial subroutine of a command. Called once when the command is
+     * initially scheduled.
+     */
+    public void initialize() {
+        startTime = System.currentTimeMillis();
+    }
+
+    // checks if power cell is in conveyor to finish or one second has gone by
+    public boolean isFinished() {
+        long currentTime = System.currentTimeMillis();
+        if (conveyorTalon.getHasSeen() || (currentTime - startTime) > maxWaitTime) {
+            return true;
+        }
+        return false;
+    }
 
 }
