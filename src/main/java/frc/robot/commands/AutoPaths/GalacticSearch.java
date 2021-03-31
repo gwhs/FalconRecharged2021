bb620130616221efc2f0c9f0364f4962306013af
zhxu033 <77419802+zhxu033@users.noreply.github.com>
2021-03-31 14:19:41 -0700

    test

--- src/main/java/frc/robot/commands/AutoPaths/GalacticSearch.java
+++ src/main/java/frc/robot/commands/AutoPaths/GalacticSearch.java
@@ -154,7 +154,7 @@ public class GalacticSearch extends SequentialCommandGroup {
     // (345, y) center of the end zone, x set to 300 for yard
     private static final double[] A6 = {180, 30}; //(180,30)
     private static final double[] B1 = {15, 60}; //(30,60)
-    private static final double[] B1Test = {12, 60}; 
+    private static final double[] B1Test = {5, 60}; 
     private static final double[] B3 = {90, 60}; 
     private static final double[] B3_Front = {140, 60}; //Robot goes forward more to avoid bumping C3 powercell
     private static final double[] B7 = {210, 60}; 
