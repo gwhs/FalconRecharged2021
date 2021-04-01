554e8aa470094efc26252e81d1132e0bef61728a
leinouye <55515800+leinouye@users.noreply.github.com>
2021-04-01 15:15:08 -0700

    bounce

--- src/main/java/frc/robot/DaphneTwoContainer.java
+++ src/main/java/frc/robot/DaphneTwoContainer.java
@@ -187,7 +187,7 @@ public class DaphneTwoContainer {
     Command autoCommand2 = new Autonomous(swerveDriveSubsystem, path2.getTrajectory(), path2.getAngle(), false);
     Command autoCommand3 = new Autonomous(swerveDriveSubsystem, path3.getTrajectory(), path3.getAngle(), false);
     Command bounceCommand = new SequentialCommandGroup(autoCommand0, autoCommand1, autoCommand2, autoCommand3);
-    //start.whenPressed(bounceCommand.withTimeout(60));
+    start.whenPressed(bounceCommand.withTimeout(60));
     //buttonX.whenPressed(autoCommand2.withTimeout(60));
 
     //Command autoCommand2 = new Autonomous(swerveDriveSubsystem, path2.getTrajectory(), path2.getAngle());
@@ -204,7 +204,7 @@ public class DaphneTwoContainer {
     //Command autoCommand4 = new Autonomous(swerveDriveSubsystem, path4.getTrajectory(), path4.getAngle(), true);
     Command galacticSearch = new GalacticSearch(swerveDriveSubsystem, intake, conveyorT);
     //Command galacticSearchCommand = new SequentialCommandGroup(autoCommand4, galacticSearch);
-    start.whenPressed(galacticSearch);//autoCommand.withTimeout(60)
+    //start.whenPressed(galacticSearch);//autoCommand.withTimeout(60)
 
 
 /*
