df718220b75c5bd32a2fba2f56bfd9b890e51aef
zhxu033 <77419802+zhxu033@users.noreply.github.com>
2021-04-01 18:15:26 -0700

    autonav

--- src/main/java/frc/robot/TrajectoryHelper.java
+++ src/main/java/frc/robot/TrajectoryHelper.java
@@ -168,8 +168,8 @@ public static double[][] testStep= {
 
      public static double[][] bounce31 = {
         {277, 32},
-        {285,90}, //11
-        {330,95}
+        {285,100}, //11
+        {330,100}
      };
         
      public static double[][] barrel = {
