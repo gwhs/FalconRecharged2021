aeb17259d54e7594e77fda444e0b930d88085edc
anton-levin <kyzhao10@gmail.com>
2021-10-02 11:06:57 -0700

    broken onecycleauto

--- src/main/java/frc/robot/DaphneTwoContainer.java
+++ src/main/java/frc/robot/DaphneTwoContainer.java
@@ -176,8 +176,8 @@ public class DaphneTwoContainer {
     buttonY.whileHeld(new ConveyorSpeed( conveyorT, .5)); //while Y is held down conveyor runs
     //buttonB.whileHeld(new IntakeSpeed(intake,-.5)); //while b is held down intake runs
 
-    // Command unoCycle = new OneCycleAuto(swerveDriveSubsystem, shooterMotor);
-    // buttonB.whenPressed(unoCycle);
+    Command unoCycle = new OneCycleAuto(swerveDriveSubsystem, shooterMotor);
+    buttonB.whenPressed(unoCycle);
     //buttonB.whenPressed(new Autonomous(swerveDriveSubsystem, path2Meters.getTrajectory(), path2Meters.getAngle(), true));
     //leftBumper.whileHeld(new ConveyorSpeed( conveyorT, -.7));
     //leftBumper.whileHeld(new SetShooterSpeed(shooterMotor, 6000));
